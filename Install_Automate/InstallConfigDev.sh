8e2e8f7fe97277adba748d832a1f4c7538a20461
Gabriel Aguiar <66374774+GabrielFerretto@users.noreply.github.com>
2022-08-13 02:36:17 -0300

    up

---
+++ Install_Automate/InstallConfigDev.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo Updating repositories..
+if ! sudo apt update;
+then
+	echo "Repositories could not be updated. Check your file /etc/apt/sources.list"
+fi
+
+ echo "Upgrade made successfully"
+ echo "Updating packages already installed"
+
+if ! sudo apt upgrade;
+then
+	echo "Erro ao autalizar"
+fi
+
+if ! sudo apt install build-essential;
+then
+	echo "Installation error check name or software requirements"
+fi	
+
+if ! sudo apt install git -y;
+then
+	echo "Installation error check name or software requirements"
+fi
+
+if ! sudo apt install terminator -y;
+then
+	echo "Installation error check name or software requirements"
+fi
+
+if !  sudo apt install python3.9 -y && sudo apt install python3-pip -y;
+then
+	echo "Installation error check name or software requirements"
+fi
+
+if !  sudo apt install virtualbox -y;
+then
+	echo "Installation error check name or software requirements"
+fi
+
+echo FNISH
+
