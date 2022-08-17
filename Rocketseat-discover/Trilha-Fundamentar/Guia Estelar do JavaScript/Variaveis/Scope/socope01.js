5b8d00fe1af319dcf9734ae04a82f55ceb195660
Gabriel Aguiar <66374774+GabrielFerretto@users.noreply.github.com>
2022-08-17 14:52:25 -0300

    Delete Rocketseat-discover/Trilha-Fundamentar/Guia Estelar do JavaScript directory

--- Rocketseat-discover/Trilha-Fundamentar/Guia Estelar do JavaScript/Variaveis/Scope/socope01.js
+++
@@ -1,9 +0,0 @@
-// var é global e também local
-
-console.log("> existe x antes do bloco ?, x")  //Ira acontecer um -> hoisting(elevação)
-
-{
-    var x = 0
-}
-
-console.log("> existe x depois do bloco ?", x)
