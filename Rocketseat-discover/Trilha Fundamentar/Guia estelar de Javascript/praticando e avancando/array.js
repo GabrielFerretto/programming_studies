9943a95a7e5ea55ed7645237be5e88de5a469994
Gabriel Aguiar <66374774+GabrielFerretto@users.noreply.github.com>
2022-08-26 14:54:17 -0300

    Estudos

---
+++ Rocketseat-discover/Trilha Fundamentar/Guia estelar de Javascript/praticando e avancando/array.js
@@ -0,0 +1,21 @@
+// Array
+
+const animals = [
+    "Lion",
+    "Monkey",
+    "Cat",
+    "Dog",
+    "Rabbit",
+    {
+        zoo: "Springfield",
+        amountOfPeople : 500,
+    }
+]
+
+// acessando valores de um array
+
+console.log(animals.length)
+console.log(animals)
+
+console.log(animals[1])
+console.log(animals[3])
---
+++ Rocketseat-discover/Trilha Fundamentar/Guia estelar de Javascript/praticando e avancando/objetos.js
@@ -0,0 +1,16 @@
+// Object
+
+const person = {
+    name: 'John',
+    age: 30,
+    weight: 88.6,
+    isAdmin: true
+}
+
+console.log(person)
+console.log(person.isAdmin)
+console.log(person.weight)
+console.log(person.age)
+console.log(person.name)
+
+console.log(`${person.name} tem ${person.age} anos`)
