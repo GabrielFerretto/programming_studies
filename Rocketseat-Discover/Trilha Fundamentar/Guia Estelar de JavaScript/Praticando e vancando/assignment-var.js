996b9a96900f1f32478301d48f563c8520d12063
Gabriel Aguiar <66374774+GabrielFerretto@users.noreply.github.com>
2022-10-26 09:51:39 -0300

    Estudos

---
+++ Rocketseat-Discover/Trilha Fundamentar/Guia Estelar de JavaScript/Praticando e vancando/assignment-var.js
@@ -0,0 +1,12 @@
+var name // declaration
+
+name = "Bilbo" //assignment
+
+console.log(name)
+
+
+let age, isHuman
+
+age = 20
+isHuman = true
+console.log(`${name} tem ${age} de idade e por mais que ele seja um hobbit ele um humano, ${isHuman}`)
