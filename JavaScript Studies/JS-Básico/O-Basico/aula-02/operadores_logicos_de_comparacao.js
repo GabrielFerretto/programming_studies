def264c20318df8b6ceac33c6bf0b5c0e6e989b0
Gabriel Aguiar <66374774+GabrielFerretto@users.noreply.github.com>
2022-11-30 09:10:30 -0300

    Estudos

---
+++ JavaScript Studies/JS-Básico/O-Basico/aula-02/operadores_logicos_de_comparacao.js
@@ -0,0 +1,27 @@
+//Operadores de comparação
+console.log(1 > 2)
+
+console.log(2 > 1)
+
+console.log(1 >= -1)
+
+console.log(1 >= 2)
+
+console.log(2 <= 1)
+
+console.log("BILBO" < "bilbo")
+
+// Operadores Logicos
+
+// AND
+console.log(true && true )
+console.log(true && true && false)
+
+// OR
+console.log(true || true)
+console.log(true || false)
+console.log(true || false || false)
+
+// NOT
+console.log(!true)
+console.log(!false)
