19090d5229fdb6082bf5b3d814483686721dcc7e
Gabriel Aguiar <66374774+GabrielFerretto@users.noreply.github.com>
2022-11-30 07:56:14 -0300

    Estudos de JS

---
+++ JavaScript Studies/JS-Básico/O-Basico/aula-01/desafio-aula-o1.js
@@ -0,0 +1,21 @@
+/*
+    Desafio:
+
+    Calcular a média aritmética de 2 ou mais números.
+
+    Fórmula: A + B + C + D / 4
+*/
+
+//Resposta 01
+
+console.log((3 + 2 + 5) / 3)
+
+//Resposta 2
+
+var a1 = 4
+var b2 = 3
+var c3 = 7
+var divisor = 3
+
+var calc = (a1 + b2 + c3) / divisor
+console.log(calc)
---
+++ JavaScript Studies/JS-Básico/O-Basico/aula-01/valores-tipos-e-operadores.js
@@ -0,0 +1,41 @@
+// TIPOS
+
+/*
+    Números:
+
+    Inteiros(3)
+    Float(3.1)
+
+*/
+console.log(7 + 7)
+
+console.log(7.5 + 7.5)
+
+console.log(50 / 2)
+
+console.log(50 * 2)
+
+console.log(50 ** 2)
+
+console.log(50.7 ** 2)
+
+// Números especiais
+console.log(Infinity)
+
+console.log(NaN) // (0 / 0)
+
+
+// Strings
+console.log("Hello Word!")
+
+console.log("Primeira linha\nSegunda linha")
+
+console.log("Primeira linha\\nSegunda linha") // "escapa" o \n (ou qualquer outro caractere especial) 
+
+console.log("con" + "ca" + "te" + "nar") // "junta" as strings
+
+console.log("2" + "2") // para o compilador, tudo entre aspas é texto.
+
+//Booleanos
+console.log(true)
+console.log(false)
