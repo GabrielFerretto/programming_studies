078c8e813f1b589509d9ab2781aa6ba6658c808e
Gabriel Aguiar <66374774+GabrielFerretto@users.noreply.github.com>
2022-12-01 09:03:43 -0300

    aula 03

---
+++ JavaScript Studies/JS-Básico/O-Basico/aula-03/declarando_variaveis.js
@@ -0,0 +1,6 @@
+var nome = "Bilbo"
+var sobrenome = "Bolseiro"
+var idade = "50"
+var cidade = "Bolsão"
+
+console.log(`Olá ${nome} ${sobrenome}, a idade  do cavalheiro é ${idade} anos,  o cavalheiro está na cidade de ${cidade}`)
---
+++ JavaScript Studies/JS-Básico/O-Basico/aula-03/desafio-aula-03.js
@@ -0,0 +1,5 @@
+var x = Number("1");
+var y = Number("2");
+var resultado = x + y;
+
+console.log(resultado)
