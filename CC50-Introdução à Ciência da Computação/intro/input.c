0cd5f6469e4bd5a423ddd12cc3975e6b496d7bd8
Gabriel Aguiar <66374774+GabrielFerretto@users.noreply.github.com>
2022-12-17 00:47:30 -0300

    C

--- CC50-Introdução à Ciência da Computação/intro/input.c
+++ CC50-Introdução à Ciência da Computação/intro/input.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <cs50.h> // Adicionar a biblioteca do CS50
 
 int main(void) {
+    // get_string é uma função específica da biblioteca do CS50!
 
-    string nome =  get_string("Digite seu nome: \n");
-    printf("Seja Bem-vindo %s \n", nome);
-    return 0;
+    string nome = get_string("Digite seu nome: \n");
+    printf("Seja Bem-vindo,%s \n",nome);
 }
