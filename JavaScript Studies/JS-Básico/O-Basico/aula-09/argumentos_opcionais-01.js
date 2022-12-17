9b9ef76f897fb24e001ed4bfada3e0bb3cf065ab
Gabriel Aguiar <66374774+GabrielFerretto@users.noreply.github.com>
2022-12-17 00:08:43 -0300

    Estudos de JavaScript

---
+++ JavaScript Studies/JS-Básico/O-Basico/aula-09/argumentos_opcionais-01.js
@@ -0,0 +1,13 @@
+var ola = function(quem) {
+    console.log("Olá " + quem);
+}
+
+ola("Bilbo") // Ola Bilbo
+
+
+/*
+    Com as funções, os parâmetros que são passados "a mais" são 
+    ignorados e os que forem passados "de
+    menos" simplesmente assumem o valor undefined.
+
+*/
---
+++ JavaScript Studies/JS-Básico/O-Basico/aula-09/argumentos_opcionais-03.js
@@ -0,0 +1,11 @@
+var ola = function(quem) {
+    if (quem == undefined) {
+        console.log("Olá tudo bem ?");
+    } else {
+        console.log("Olá tudo bem " + quem + "?");
+    }
+}
+
+ola();
+
+ola("Bilbo");
