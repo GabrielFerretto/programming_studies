74742fa93b69928b6d9db0cc0336ba4670074052
Gabriel Aguiar <66374774+GabrielFerretto@users.noreply.github.com>
2023-01-07 13:42:33 -0300

    Estudos

---
+++ JavaScript studies/JS-basico/input-output.js
@@ -0,0 +1,6 @@
+var nome = prompt("Digite seu Nome: ");
+var sobreNome = prompt("Digite seu Sobre Nome");
+
+var nomeCompleto =  nome + sobreNome;
+
+console.log(nomeCompleto);
---
+++ JavaScript studies/JS-basico/var-02.js
@@ -0,0 +1,12 @@
+var a = 100;
+var A = 50;
+
+var adicao = a + A;
+var subtracao = a - A;
+var divsao = a / A;
+var multiplicacao = a * A;
+
+console.log(adicao);
+console.log(subtracao);
+console.log(divsao);
+console.log(multiplicacao);
